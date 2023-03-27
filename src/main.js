847e5037c40471fc8c24ae22af07e7e7ee8248b0
unknown <jr747524@gmail.com>
2023-03-27 18:29:36 -0500

    functions filter

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
-// import data from './data/data.json';
-// import { datos }  from './data/datos.js';
-const url = './data/datos.json'
+
+
+const botton = document.querySelector('button_players');
+const menu = document.querySelector('menu');
+const input = document.querySelector('input');
+botton.addEventListener('click', (e) => {
+    e.preventDefault();
+    data(input.value);
+})
 
 // console.log(datos);
+function data(Jugador){
+    fetch(`./data/datos.json/${Jugador}`).then(response => response.json()).then(data => createPlayers(data)).catch(error => console.error(error));
+    
+    createPlayers(Jugador);
+}
 
-fetch(url).then(response => response.json()).then(data => console.log(data)).catch(error => console.error(error));
-document.createElement('div')
-document.createAttribute('#', 'id')
+function createPlayers(Jugador) {
+    const img = document.createElement('img');
+    img.src = Jugador.image;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = Jugador.name;
+
+    const div = document.createElement('div');
+    div.appendChild(img);
+    div.appendChild(h3);
+
+    menu.appendChild(div)
+}
